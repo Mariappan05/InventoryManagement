af3a6ffd9a47e204456ba613607669a51203b511
Mariappan05 <rammariappan05@gmail.com>
2024-09-10 01:53:39 +0530

    Inventorymanagementsystem UI added

---
+++ InventoryManagementSystem/InventoryManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Collections.Generic;
+using InventoryManagementSystem.Models;
+
+namespace Inventory_Management_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("Product")]
+        public IActionResult Product()
+        {
+            return View();
+        }
+        [HttpGet("Stock")]
+        public IActionResult Stock()
+        {
+            return View();
+        }
+        [HttpGet("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Rolemanagement()
+        {
+            return View();
+        }
+
+    }
+}
